453392e741a74d316b229493094578b3f9ab8831
mohamed <2000gaber2000@gmail.com>
2025-04-06 13:02:58 +0200

    btn-scroll

--- src/app/navbar/Navbar.component.ts
+++ src/app/navbar/Navbar.component.ts
@@ -1,18 +1,34 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, AfterViewInit, Renderer2 } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
-selector:'Navbar-view',
-standalone:true,
-imports:[RouterModule,CommonModule],
-templateUrl:'./Navbar.component.html',
-styleUrls:['./Navbar.component.css','../../styles.css']
+  selector: 'Navbar-view',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './Navbar.component.html',
+  styleUrls: ['./Navbar.component.css', '../../styles.css']
 })
-export class Navbar{
+export class Navbar implements AfterViewInit {
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const links = document.querySelectorAll('.side-link');
+
+    links.forEach(link => {
+      this.renderer.listen(link, 'click', () => {
+        this.hideSidebar();
+      });
+    });
+  }
+
   toggleMenu() {
     document.querySelector('.sidebar')?.classList.toggle('active');
-    console.log("seee");
+    console.log("Sidebar toggled");
+  }
 
+  hideSidebar() {
+    document.querySelector('.sidebar')?.classList.remove('active');
+    console.log("Sidebar hidden");
   }
 }
